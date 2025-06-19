144d810f4674497e49ed662a2ba78a4851bc27d2
Ymfbar <radib14@gmail.com>
2025-06-20 03:22:45 +0700

    bg login

--- auth/login.php
+++ auth/login.php
@@ -43,6 +43,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
+            background: url('../assets/gudanglogin.jpg') no-repeat center center fixed;
+            background-size: cover;
+            min-height: 100vh;
+
             display: flex;
             align-items: center;
             justify-content: center;
